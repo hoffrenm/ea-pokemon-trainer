ffd9db2ca19ef8782be2faa1cd21d75cd3034563
Mika Hoffren <mika.hoffren@helsinki.fi>
2023-02-03 16:06:59 +0200

    Add logout button

--- src/app/profile/profile-info/profile-info.component.ts
+++ src/app/profile/profile-info/profile-info.component.ts
@@ -1,4 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageKeys } from 'src/app/enums/storage-keys.enum';
+import { LoginService } from 'src/app/services/login.service';
+import { TrainerService } from 'src/app/services/trainer.service';
+import { StorageUtils } from 'src/app/utils/storage.utils';
 
 @Component({
   selector: 'app-profile-info',
@@ -6,15 +11,19 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./profile-info.component.css']
 })
 export class ProfileInfoComponent implements OnInit {
-  @Input() trainerName: string | undefined = ""
+  @Input() trainerName: string | undefined = "";
 
-  constructor() {
-
-  }
+  constructor(
+    private readonly router: Router,
+    private readonly trainerService: TrainerService) { }
 
   ngOnInit(): void {
 
   }
 
-
+  public handleLogout(): void {
+    this.trainerService.trainer = undefined;
+    StorageUtils.storageClearKey(StorageKeys.Trainer)
+    this.router.navigateByUrl('/login');
+  }
 }
